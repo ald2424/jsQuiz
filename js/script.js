0452c7af9a4b490eacf7224828d191c07edd2eda
ald2424 <abigaild2424@gmail.com>
2019-11-09 18:12:42 -0500

    Added score calculations

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 // TASKS:
-// Once quiz is over, veiw score button appears <--Hide button until quiz is over OR make button after quiz is
-// When user clicks view score, the score is calculated and displayed on screen (not as an alert)
-// Add timer
-// If the user gets the question wrong, the timer loses 10 seconds
-// The faster the user answers, the higher the score (Need to figure out the math here)
-// quiz ends either after all questions are answered or timer runs out
 // User can chose to have their initials saved with their score in local storage
 // Add view high scores button to start of quiz
 // **** BONUS **** instead of start button, create three quizzes - easy, medium, and hard for user to select from
@@ -45,15 +39,17 @@ var displayAnswers = document.querySelector("#a");
 var startB = document.querySelector("#startB");
 var result = document.querySelector("#result");
 var timerEl = document.querySelector("#timer");
+var message = document.querySelector("#message");
+var scoreBoard = document.querySelector("#scoreBoard");
 var score = 0;
 var answers;
 
 
-
+// Timer Code
 var secondsLeft = 0;
 var ended = false;
 function setTimer(){
-  secondsLeft = 50;
+  secondsLeft = (questions.length * 10);
   var timerInt = setInterval(function(){
     secondsLeft--;
     timerEl.textContent = "Time: " + secondsLeft;
@@ -62,6 +58,7 @@ function setTimer(){
     }
     if (secondsLeft == 0){
       clearInterval(timerInt);
+      message.textContent = "Time's Up!!"
       showResults();
     }
   }, 1000);
@@ -123,8 +120,17 @@ displayAnswers.addEventListener("click", function(event){
     score++;
   }
   else{
+    // If the user answers wrong, this code checks to see if there is at least 10 seconds left.
+    // If there is over 10 seconds left, 10 seconds are deducted from timer. If there is less, the quiz ends
+      if(secondsLeft > 11){
     secondsLeft = secondsLeft - 10;
     alert("bad");
+      }
+      else{
+      ended = true;
+      message.textContent = "You got the answer wrong with less than 10 seconds left!! Time's up!!"
+      showResults();
+      }
   }
 
   questionCounter++;
@@ -152,5 +158,23 @@ result.appendChild(showScore);
 
 // Runs when calculate score button is pressed
 document.getElementById("score").onclick = function (quiz){
-alert(score);
+score = score * 10;
+var timeBonus = 0;
+if(secondsLeft >= 40){
+  timeBonus = 50;
+}
+else if(secondsLeft >= 30 && secondsLeft < 40){
+  timeBonus = 40;
+}
+else if(secondsLeft >= 20 && secondsLeft < 30){
+  timeBonus = 30;
+}
+else if(secondsLeft >= 10 && secondsLeft < 20){
+  timeBonus = 20;
+}
+else{
+  timeBonus = 0;
+}
+var final = score + timeBonus;
+scoreBoard.textContent = "Your score is " + final;
 }
